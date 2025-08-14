374609797d63f73d3988a94ecfe024610f6d8a24
Anayatul Ahad Shoikot <shoikotofficial@gmail.com>
2025-08-14 16:51:06 +0600

    05 solved

---
+++ 05_capitalized_every_word.js
@@ -0,0 +1,13 @@
+let sentence = "My name is Ahad. and I am a VERY good boy. i Can do this";
+
+sentence = sentence
+    .split(" ")
+    .map( word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ")
+
+console.log(sentence);
+
+
+
+
+
